399f35bf682c108966a2ff69076d0ecc9894fa45
JuanPZ2000 <juanpablozuluaga2000@hotmail.com>
2021-09-28 17:09:32 -0500

    UwU

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import Corners as Cn
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 if __name__ == '__main__':
+    # Se crea la clase
     quadrilateral = Quadrilateral(500)
+    # Se genera la imagen
     imagen = quadrilateral.generate()
     cv2.imshow("imagen generada",imagen)
+    # Se realiza Canny
     high_thresh = 300
     bw_edges = cv2.Canny(imagen, high_thresh*0.3, high_thresh, L2gradient=True)
+    # Se realiza el metodo de detectar esquinas
     imagenFinal, coordenadas =Cn.DetectCorners(imagen, bw_edges)
     print("Las coordenadas de los bordes encontrados fueron x,y:",coordenadas)
     cv2.imshow('Imagen con bordes', imagenFinal)
